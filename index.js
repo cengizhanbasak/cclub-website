f756cffaa36fa02b72530ea1df3a52230aa45426
Tosunami <cengizhanbasakk@gmail.com>
2018-04-29 22:42:49 +0300

    Basic PostgreSQL Integration with active_member and superUser Models
    To integrate:
    * install postgresql
    * create a superuser role and a database named "cclub"
    * if want to create with a different name, change client info from index.js file
    * log into created database in postgresql, create tables for models with:
    
    - CREATE TABLE active_members (
        id smallserial NOT NULL,
        name varchar(30) NOT NULL,
        timestamp timestamp default current_timestamp);
    - CREATE TABLE superUsers (
        username varchar(25) NOT NULL,
        password TEXT NOT NULL);
    
    * to add a superuser, inside cclub database as superuser:
    - INSERT INTO superUsers ( username, password ) VALUES ( '<desired username>', '<desired_password>' )
    
    issues:
    - all superuser system works under a global variable superUser, once an admin signs in, everyone has access to admin panel.

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+const { Client } = require('pg')
+const client = new Client({
+  user: 'cclub',
+  host: '/var/run/postgresql',
+  database: 'cclub',
+  password: '',
+  port: 5432,
+})
+client.connect()
+
 app.use( express.static(__dirname + '/public' , { maxage: '1d' }) );
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
 var active;
 
+var superUser = false;
 
 // GET REQUESTS
 
@@ -19,6 +36,72 @@ app.get('/',function(req,res){
   res.setHeader("Cache-Control", "public, max-age=2592000");
   res.render('./src/index.ejs',{active:''});
 });
+
+// ADMIN STUFF
+
+app.get('/admin',function(req,res){
+  if(superUser == true){
+    client.query("SELECT * FROM active_members",(err,result) => {
+      res.render('./src/admin.ejs',{members: result.rows})
+    })
+  }else {
+    res.redirect('/')
+  }
+});
+
+app.get('/admin/login',function(req,res){
+  res.render('./src/adminLogin.ejs');
+});
+
+app.post('/admin/login',function(req,res){
+  var username = req.body.username;
+  var password = req.body.password;
+
+  client.query("SELECT EXISTS(SELECT * FROM superusers WHERE username='"+ username +"')",(err,result) => {
+    console.log(result.rows[0].exists)
+    if( result.rows[0].exists == true ){
+      client.query("SELECT * FROM superusers WHERE username='"+ username +"'", (err, res1) => {
+        if( password == res1.rows[0].password ){
+          superUser = true;
+          res.redirect('/admin')
+        }else {
+          res.redirect('/')
+        }
+      })
+    }else {
+      res.redirect('/')
+    }
+  })
+});
+
+app.get('/admin/logout',function(req,res){
+  superUser = false;
+  res.redirect('/')
+});
+
+// NEW MEMBER
+
+app.get('/members/new',function(req,res){
+  if(superUser == true){
+    res.render('./src/members/new.ejs')
+  }else {
+    res.redirect('/')
+  }
+})
+
+app.post('/members/new',function(req,res){
+  if(superUser == true){
+    var name = req.body.name
+    if(name != '')
+    client.query("INSERT INTO active_members ( name ) VALUES ( '" + name + "' )",(err,result) => {
+      res.redirect('/admin')
+    })
+  }else {
+    res.redirect('/')
+  }
+})
+
+// ADMIN STUFF END
 
 app.get('/announce',function(req,res){
   res.render('./src/announce.ejs',{active:'announce'});
